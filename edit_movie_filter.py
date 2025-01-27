23bf7211b7c8909e5d07abccaaae599718513b65
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-01-27 11:16:45 +0000

    a

--- edit_movie_filter.py
+++ edit_movie_filter.py
@@ -1,11 +1,11 @@
 from database import Database
 from models.genres import Genre
-
+import operator
 db = Database()
 movies = db.fetch_movies()
 print(f'{movies=}')
-movie_id: int = 41
-data: dict[str, str] = list(filter(lambda x: x['movie_id'] == movie_id, movies))[0]
+movie_id: int = 45
+data: dict[str, str] = list(filter(lambda x: operator.eq(movie_id, x['movie_id'] ), movies))[0]
 print(f'{data=}')
 genres: list[str] = data.get('genres').split(Genre.genre_split())
 title: str = data.get('title')
