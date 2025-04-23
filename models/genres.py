0c077339663c2c29be57786bbd5a01de7575edc1
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-04-23 16:30:56 +0100

    a

--- models/genres.py
+++ models/genres.py
@@ -1,9 +1,13 @@
 from PyQt6.QtWidgets import QCheckBox
-from pydantic import BaseModel
+from pydantic import BaseModel, NonNegativeInt, field_validator
+
 
 class Genre(BaseModel):
+    class Config:
+        frozen = True
+
     name: str
-    genre_id: int
+    genre_id: NonNegativeInt
 
     @staticmethod
     def genre_split():
@@ -16,3 +20,14 @@ class Genre(BaseModel):
     @staticmethod
     def create_genre_checkboxes(self, db):
         return [QCheckBox(genre.name, self) for genre in Genre.get_genres(db)]
+
+    @field_validator('name')
+    @classmethod
+    def validate_name(cls, name: str):
+        if name.strip() == '':
+            raise Exception('Genre cannot be empty')
+        return name
+
+
+genre = Genre(name='', genre_id=0)
+print(genre.model_dump())
